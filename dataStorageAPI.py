ca4534b0fbc2bedcd8704c635fe559e26d3f4abb
Invariance-NaN <quinnydf@yahoo.ca>
2023-07-02 14:51:46 -0400

    Use `.ok` to check for request validity

--- dataStorageAPI.py
+++ dataStorageAPI.py
@@ -9,25 +9,26 @@ class DataStorageAPI:
     self.session.get("https://data-storage-system.danielchen1464.repl.co")
 
   def getKeys(self):
-    req = self.session.get(
+    res = self.session.get(
       "https://data-storage-system.danielchen1464.repl.co/get_keys?password={passcode}"
         .format(passcode = os.environ[self.passwordStringKey])
       )
 
-    if req.ok:
-      return str(req.content)
+    if res.ok:
+      return str(res.content)
     else:
       raise DataAPIException
 
   def getValue(self,key: str,evaluate: bool = False):
-    data = self.session.get("https://data-storage-system.danielchen1464.repl.co/database?password={passcode}&key={inputKey}".format(passcode = os.environ[self.passwordStringKey], inputKey = key))
-    if str(data) == "<Response [200]>":
+    res = self.session.get("https://data-storage-system.danielchen1464.repl.co/database?password={passcode}&key={inputKey}".format(passcode = os.environ[self.passwordStringKey], inputKey = key))
+
+    if res.ok:
       if evaluate:
         # the [2:-1] is nessecary because all content comes out like b'something_here', and the [2:-1] removes all of that.
         # .strip removes the pesky newline characters from the string
-        return ast.literal_eval(str(data.content).strip()[2:-1])
+        return ast.literal_eval(str(res.content).strip()[2:-1])
       else:
-        return data.content[2:-1]
+        return res.content[2:-1]
     else:
       raise DataAPIException
       return
@@ -35,15 +36,17 @@ class DataStorageAPI:
   def setValue(self,key:str,value):
     input = str(value)
 
-    data = self.session.post("https://data-storage-system.danielchen1464.repl.co/database?password={passcode}&key={inputKey}".format(passcode = os.environ[self.passwordStringKey], inputKey = key), data = {"value":input})
-    if str(data) == "<Response [200]>":
-      return data.content[2:-1]
+    res = self.session.post("https://data-storage-system.danielchen1464.repl.co/database?password={passcode}&key={inputKey}".format(passcode = os.environ[self.passwordStringKey], inputKey = key), data = {"value":input})
+
+    if res.ok:
+      return res.content[2:-1]
     else:
       raise DataAPIException
 
   def delValue(self,key:str):
-    data = self.session.get("https://data-storage-system.danielchen1464.repl.co/delete_key?password={passcode}&key={inputKey}".format(passcode = os.environ[self.passwordStringKey], inputKey = key))
-    if str(data) == "<Response [200]>":
-      return data.content[2:-1]
+    res = self.session.get("https://data-storage-system.danielchen1464.repl.co/delete_key?password={passcode}&key={inputKey}".format(passcode = os.environ[self.passwordStringKey], inputKey = key))
+
+    if res.ok:
+      return res.content[2:-1]
     else:
       raise DataAPIException
